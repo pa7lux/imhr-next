8833ab625406157dcd21ebf501982dd39bc1622a
Illya <sypersys228@gmail.com>
2022-11-16 19:11:00 +0100

    imports fix

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,7 @@
 import { createContext } from 'react'
 import '../styles/globals.css';
 import type { AppProps } from 'next/app';
-import { Layout } from '../components/Layout/Layout';
+import { Layout } from '../components/Layout/layout';
 import { ThemeProvider } from '../store/context/context';
 
 export default function App({ Component, pageProps }: AppProps) {
