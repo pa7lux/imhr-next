eec334b0b36edc09a83f13368837316a17d9e89b
pa7lux <pa7lux@gmail.com>
2023-02-11 22:33:40 +0400

    fixes to sitemap generator

--- pages/sitemap.xml.tsx
+++ pages/sitemap.xml.tsx
@@ -7,7 +7,7 @@ const Sitemap = () => {
 };
 
 export const getServerSideProps: GetServerSideProps = async ({ res }) => {
-  const BASE_URL = 'http://localhost:3000';
+  const BASE_URL = 'https://imhr.top';
   const locales = ['ru', 'en', 'uk'];
 
   const staticPaths = fs
@@ -15,15 +15,27 @@ export const getServerSideProps: GetServerSideProps = async ({ res }) => {
     .filter((staticPage) => {
       return ![
         'api',
-        '_app.js',
-        '_document.js',
-        '404.js',
-        'sitemap.xml.js',
+        '_app.tsx',
+        '_document.tsx',
+        '404.tsx',
+        'sitemap.xml.tsx',
+        'index.tsx',
+        'posts',
       ].includes(staticPage);
     })
     .map((staticPagePath) => {
-      return `${BASE_URL}/${staticPagePath}`;
+      let trimmedPath = staticPagePath.replace('.tsx', '');
+      return [
+        `${BASE_URL}/${trimmedPath}`,
+        `${BASE_URL}/ru/${trimmedPath}`,
+        `${BASE_URL}/en/${trimmedPath}`,
+      ];
     });
+
+  const staticPathArray = staticPaths.reduce((acc, item) => {
+    item.forEach((el) => acc.push(el));
+    return acc;
+  }, []);
 
   const files = fs.readdirSync(`${process.cwd()}/data/posts/uk`);
 
@@ -48,10 +60,15 @@ export const getServerSideProps: GetServerSideProps = async ({ res }) => {
     return `${BASE_URL}/${article.locale}/${article.params.slug}`;
   });
 
-  const allPaths = [...staticPaths, ...dynamicPaths];
+  const allPaths = [
+    BASE_URL,
+    `${BASE_URL}/en/`,
+    `${BASE_URL}/ru/`,
+    ...staticPathArray,
+    ...dynamicPaths,
+  ];
 
-  const sitemap = `
-    <?xml version="1.0" encoding="UTF-8"?>
+  const sitemap = `<?xml version="1.0" encoding="UTF-8"?>
     <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
       ${allPaths
         .map((url) => {
